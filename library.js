d6b8f6296602c70715d3bbf2b68c69d907fccc07
TFrank <madmonsterHI@gmail.com>
2023-07-13 17:44:49 -0400

    working on pop up form

--- library.js
+++ library.js
@@ -1,3 +1,11 @@
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+}
+
+function closeForm() {
+document.getElementById("popupForm").style.display = "none";
+}
+
 let table = document.getElementById("myTable");
 
 let myLibrary = [];
